aed294cc3fc9714bb694f0207ba6a79cc6499b72
Kelly Selden <kellyselden@gmail.com>
2017-08-27 16:06:55 -0700

    move some acceptance tests to integration tests to save time

---
+++ test/helpers/build-tmp.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs-extra');
+const gitFixtures = require('git-fixtures');
+const run = require('../../src/run');
+
+const gitInit = gitFixtures.gitInit;
+const commit = gitFixtures.commit;
+const postCommit = gitFixtures.postCommit;
+
+module.exports = function buildTmp(
+  fixturesPath,
+  tmpPath,
+  dirty
+) {
+  gitInit({
+    cwd: tmpPath
+  });
+
+  let tags = fs.readdirSync(fixturesPath);
+
+  for (let i = 0; i < tags.length; i++) {
+    if (i !== 0) {
+      run('git rm -r *', {
+        cwd: tmpPath
+      });
+    }
+
+    let tag = tags[i];
+
+    fs.copySync(path.join(fixturesPath, tag), tmpPath);
+
+    commit({
+      m: tag,
+      tag,
+      cwd: tmpPath
+    });
+  }
+
+  postCommit({
+    cwd: tmpPath,
+    dirty
+  });
+};
