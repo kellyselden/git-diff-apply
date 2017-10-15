b663ad33ac048aded5c8ce87652371c5349e6af1
Kelly Selden <kellyselden@gmail.com>
2017-10-15 08:37:14 -0700

    add cwd support to functions

--- src/get-checked-out-branch-name.js
+++ src/get-checked-out-branch-name.js
@@ -2,9 +2,9 @@
 
 const run = require('./run');
 
-module.exports = function getCheckedOutBranchName() {
+module.exports = function getCheckedOutBranchName(options) {
   // "git branch" doesn't show orphaned branches
-  let str = run('git symbolic-ref --short HEAD');
+  let str = run('git symbolic-ref --short HEAD', options);
 
   return str.replace(/\r?\n/g, '');
 };
--- src/is-git-clean.js
+++ src/is-git-clean.js
@@ -2,6 +2,6 @@
 
 const run = require('./run');
 
-module.exports = function() {
-  return !run('git status --porcelain');
+module.exports = function isGitClean(options) {
+  return !run('git status --porcelain', options);
 };
