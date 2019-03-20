65bd293ed9f58ae43129df73b48962d793461cdc
Kelly Selden <kellyselden@gmail.com>
2019-03-20 16:28:57 -0700

    Revert "don't delete cwd (because monorepo)"
    This reverts commit 43267e511217d1786fe8c85c2d7123ab4cb3d8f0.

--- src/git-remove-all.js
+++ src/git-remove-all.js
@@ -3,12 +3,5 @@
 const run = require('./run');
 
 module.exports = function gitRemoveAll(options) {
-  // this removes cwd as well, which trips up your terminal
-  // when in a monorepo
-  // run('git rm -r . -f', options);
-
-  let files = run('git ls-files', options).trim();
-  for (let file of files.split(/\r?\n/g)) {
-    run(`git rm -rf ${file}`, options);
-  }
+  run('git rm -r . -f', options);
 };
