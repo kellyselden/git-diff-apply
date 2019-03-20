dca0700768ed16e1e42a8003c1a317579e32c86f
Kelly Selden <kellyselden@gmail.com>
2019-03-20 15:10:50 -0700

    don't delete cwd (because monorepo)

--- src/git-remove-all.js
+++ src/git-remove-all.js
@@ -3,5 +3,12 @@
 const run = require('./run');
 
 module.exports = function gitRemoveAll(options) {
-  run('git rm -r . -f', options);
+  // this removes cwd as well, which trips up your terminal
+  // when in a monorepo
+  // run('git rm -r . -f', options);
+
+  let files = run('git ls-files', options).trim();
+  for (let file of files.split(/\r?\n/g)) {
+    run(`git rm -rf ${file}`, options);
+  }
 };
