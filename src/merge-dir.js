538d0d4b2d55af4a5569a46e70ad98aaef76b361
Kelly Selden <kellyselden@gmail.com>
2019-04-07 19:21:22 +0100

    use fs-move instead of klaw

--- src/merge-dir.js
+++ src/merge-dir.js
@@ -2,7 +2,7 @@
 
 const co = require('co');
 const fs = require('fs-extra');
-const klaw = require('klaw');
+const move = require('fs-move');
 const copyRegex = require('./copy-regex');
 const debug = require('debug')('git-diff-apply');
 
@@ -12,33 +12,14 @@ module.exports = co.wrap(function* mergeDir(from, to) {
   }
 
   debug(`mergeDir ${from} ${to}`);
-  yield new Promise((resolve, reject) => {
-    let promises = [];
-    klaw(from, {
-      preserveSymlinks: true,
-      filter(src) {
-        return copyRegex.test(src);
-      }
-    }).on('data', item => {
-      let fromFile = item.path;
-      let toFile = to + item.path.substr(from.length);
+  yield move(from, to, {
+    merge: true,
+    overwrite: true,
+    filter(fromFile, toFile) {
       debug(`from ${fromFile}`);
       debug(`to ${toFile}`);
 
-      let promise;
-      if (item.stats.isDirectory()) {
-        promise = fs.ensureDir(toFile);
-      } else {
-        // `fs.move` doesn't handle broken symlinks
-        promise = fs.rename(fromFile, toFile);
-      }
-      promises.push(promise);
-    }).on('error', (err, item) => {
-      debug(`error ${item.path}`);
-
-      reject(err);
-    }).on('end', () => {
-      resolve(Promise.all(promises));
-    });
+      return copyRegex.test(fromFile);
+    }
   });
 });
