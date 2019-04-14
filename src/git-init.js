557f91d1f78c7980fbbca02ca3762137215f9707
Kelly Selden <kellyselden@gmail.com>
2019-04-14 11:38:47 +0100

    ignore global .gitignore when creating temp repos

--- src/git-init.js
+++ src/git-init.js
@@ -6,4 +6,7 @@ module.exports = function gitInit(options) {
   run('git init', options);
   run('git config user.email "you@example.com"', options);
   run('git config user.name "Your Name"', options);
+
+  // ignore any global .gitignore that will mess with us
+  run('git config --local core.excludesfile false', options);
 };
