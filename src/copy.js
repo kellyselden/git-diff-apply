860a472da22ca8ecf31585cdb218d63b9d302442
Kelly Selden <kellyselden@gmail.com>
2019-05-19 14:41:04 +0100

    add async code

--- src/copy.js
+++ src/copy.js
@@ -1,18 +1,19 @@
 'use strict';
 
+const co = require('co');
 const fs = require('fs-extra');
 const copyRegex = require('./copy-regex');
 const debug = require('debug')('git-diff-apply');
 
-module.exports = function copy(from, to) {
+module.exports = co.wrap(function* copy(from, to) {
   if (debug.enabled) {
-    debug(require('fs').readdirSync(from));
+    debug(yield fs.readdir(from));
   }
 
   debug(`copy ${from} ${to}`);
-  return fs.copy(from, to, {
+  yield fs.copy(from, to, {
     filter(src) {
       return copyRegex.test(src);
     }
   });
-};
+});
