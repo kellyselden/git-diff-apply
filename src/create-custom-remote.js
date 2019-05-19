eb2c61a153c300bc66c76257ca66d660aa230f9d
Kelly Selden <kellyselden@gmail.com>
2019-05-19 13:57:28 +0100

    remove denodeify

--- src/create-custom-remote.js
+++ src/create-custom-remote.js
@@ -5,8 +5,8 @@ const run = require('./run');
 const gitInit = require('./git-init');
 const commitAndTag = require('./commit-and-tag');
 const gitRemoveAll = require('./git-remove-all');
-const denodeify = require('denodeify');
-const tmpDir = denodeify(require('tmp').dir);
+const { promisify } = require('util');
+const tmpDir = promisify(require('tmp').dir);
 
 module.exports = co.wrap(function* createCustomRemote({
   startCommand,
