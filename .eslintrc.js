3166c22c3c4219102a6d9f73217b16b0eb1aba45
Kelly Selden <kellyselden@gmail.com>
2019-07-22 11:58:25 +0100

    update linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,11 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018
   },
-  plugins: [
-    'node'
-  ],
   extends: [
-    'sane',
-    'plugin:node/recommended'
+    'sane-node'
   ],
   env: {
-    es6: true,
-    node: true
+    es6: true
   },
   rules: {
     // https://github.com/eslint/eslint/issues/11899
@@ -30,11 +25,14 @@ module.exports = {
       plugins: [
         'mocha'
       ],
+      extends: [
+        'plugin:mocha/recommended'
+      ],
       env: {
         mocha: true
       },
       rules: {
-        'mocha/no-exclusive-tests': 'error'
+        'mocha/no-setup-in-describe': 0
       }
     }
   ]
