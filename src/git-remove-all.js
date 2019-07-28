dcbbbab49af75ed6fd3b3a36af94ab5fa85587a2
Kelly Selden <kellyselden@gmail.com>
2019-07-28 11:01:04 +0100

    up maxBuffer on ls-files

--- src/git-remove-all.js
+++ src/git-remove-all.js
@@ -4,12 +4,21 @@ const fs = require('fs-extra');
 const path = require('path');
 const run = require('./run');
 
+async function lsFiles(options) {
+  // attempt to fix https://github.com/ember-cli/ember-cli-update/issues/583
+  options = { maxBuffer: 500 * 1024, ...options };
+
+  let files = (await run('git ls-files', options)).split(/\r?\n/g);
+
+  return files;
+}
+
 module.exports = async function gitRemoveAll(options) {
   // this removes cwd as well, which trips up your terminal
   // when in a monorepo
   // await run('git rm -rf .', options);
 
-  let files = (await run('git ls-files', options)).split(/\r?\n/g);
+  let files = await lsFiles(options);
 
   for (let file of files) {
     // this removes folders that become empty,
