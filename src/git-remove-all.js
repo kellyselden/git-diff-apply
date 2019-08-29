77d6d8d5b8fd87f55d69388262eaeaf3ab41750c
Ethan Langevin <ethan@better.com>
2019-08-29 10:53:30 -0400

    pr notes: Keep lsFiles as a separate function

--- src/git-remove-all.js
+++ src/git-remove-all.js
@@ -5,16 +5,23 @@ const path = require('path');
 const run = require('./run');
 const { runWithSpawn } = require('./run');
 
+async function lsFiles(options) {
+  // Using spawn rather than run here because ls-files produces a lot
+  // of output if it gets run in a large repo.
+  // See https://github.com/kellyselden/git-diff-apply/pull/277
+  let files = (await runWithSpawn('git', ['ls-files'], options))
+    .split(/\r?\n/g)
+    .filter(Boolean);
+
+  return files;
+}
+
 module.exports = async function gitRemoveAll(options) {
   // this removes cwd as well, which trips up your terminal
   // when in a monorepo
   // await run('git rm -rf .', options);
 
-  // Using spawn rather than run here because ls-files produces a lot
-  // of output if it gets run in a large repo
-  let files = (await runWithSpawn('git', ['ls-files'], options))
-    .split(/\r?\n/g)
-    .filter(Boolean);
+  let files = await lsFiles(options);
 
   for (let file of files) {
     // this removes folders that become empty,
