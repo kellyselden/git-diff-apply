9635a76ca4a0d30283b0f17a3b1dd92c1a31e015
Kelly Selden <kellyselden@gmail.com>
2020-01-01 20:31:47 +0000

    allow empty commits

--- src/commit.js
+++ src/commit.js
@@ -1,14 +1,12 @@
 'use strict';
 
 const run = require('./run');
-const { isGitClean } = require('./git-status');
 
 module.exports = async function commit(options) {
   await run('git add -A', options);
 
-  // it's possible for there to be no changes between tags
-  if (!await isGitClean(options)) {
-    // run with --no-verify to skip pre-commit application level hooks
-    await run('git commit -m "message" --no-verify', options);
-  }
+  // run with --no-verify to skip pre-commit application level hooks
+  // --allow-empty because either their is no files for the first commit,
+  // or the second commit has no changes
+  await run('git commit --allow-empty -m "message" --no-verify', options);
 };
