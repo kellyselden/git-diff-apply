06c811065361d8bfce38df6c38e00ea978718c7e
Kelly Selden <kellyselden@gmail.com>
2020-01-04 12:12:00 +0000

    always pipe instead of using a bool

--- src/resolve-conflicts.js
+++ src/resolve-conflicts.js
@@ -3,13 +3,9 @@
 const { spawn } = require('child_process');
 const debug = require('debug')('git-diff-apply');
 
-module.exports = function resolveConflicts({
-  shouldPipe
-}) {
+module.exports = function resolveConflicts() {
   debug('git mergetool');
-  // we need to print it to the host's console
-  // or make it available for piping
-  return spawn('git', ['mergetool'], {
-    stdio: shouldPipe ? 'pipe' : 'inherit'
-  });
+
+  // pipe for those using as a library can interact
+  return spawn('git', ['mergetool']);
 };
