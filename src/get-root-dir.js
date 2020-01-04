41f1961cfd2e9301676994dc733e51acb4f87083
Kelly Selden <kellyselden@gmail.com>
2020-01-04 11:31:58 +0000

    remove cwd assumption from library

--- src/get-root-dir.js
+++ src/get-root-dir.js
@@ -2,7 +2,7 @@
 
 const run = require('./run');
 
-module.exports = async function getRootDir() {
-  let root = (await run('git rev-parse --show-toplevel')).trim();
+module.exports = async function getRootDir(options) {
+  let root = (await run('git rev-parse --show-toplevel', options)).trim();
   return root;
 };
--- src/resolve-conflicts.js
+++ src/resolve-conflicts.js
@@ -3,9 +3,9 @@
 const { spawn } = require('child_process');
 const debug = require('debug')('git-diff-apply');
 
-module.exports = function resolveConflicts() {
+module.exports = function resolveConflicts(options) {
   debug('git mergetool');
 
   // pipe for those using as a library can interact
-  return spawn('git', ['mergetool']);
+  return spawn('git', ['mergetool'], options);
 };
