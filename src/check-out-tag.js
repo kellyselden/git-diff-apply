a275a57899aa53e399bc29d19d974c095cbc2e7b
Kelly Selden <kellyselden@gmail.com>
2020-01-04 10:40:07 +0000

    remove --mirror from git clone
    not sure why this was every needed?

--- src/check-out-tag.js
+++ src/check-out-tag.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const path = require('path');
 const run = require('./run');
 
 module.exports = async function checkOutTag(repoDir, tag) {
-  let gitDir = path.join(repoDir, '.git');
-
-  let sha = await run(`git --git-dir="${gitDir}" rev-parse ${tag}`);
-  await run(`git --git-dir="${gitDir}" --work-tree="${repoDir}" checkout ${sha.trim()}`);
+  let sha = await run(`git rev-parse ${tag}`, { cwd: repoDir });
+  await run(`git checkout ${sha.trim()}`, { cwd: repoDir });
 };
