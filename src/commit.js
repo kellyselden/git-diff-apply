85414ace27f69e708e8c82db63ac5b5746af5b80
Kelly Selden <kellyselden@gmail.com>
2020-01-06 11:50:31 +0000

    add commit message

--- src/commit.js
+++ src/commit.js
@@ -3,11 +3,11 @@
 const run = require('./run');
 const utils = require('./utils');
 
-module.exports = async function commit(options) {
+module.exports = async function commit(message, options) {
   await run('git add -A', options);
 
   // run with --no-verify to skip pre-commit application level hooks
   // --allow-empty because either their is no files for the first commit,
   // or the second commit has no changes
-  await utils.run('git commit --allow-empty -m "message" --no-verify', options);
+  await utils.run(`git commit --allow-empty -m "${message}" --no-verify`, options);
 };
